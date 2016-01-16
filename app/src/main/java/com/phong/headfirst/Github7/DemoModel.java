506b92898998f22993c12edeb5f497435dd6eae5
p7157568 <p7157568@gmail.com>
2016-01-16 07:22:31 +0700

    hello

---
+++ app/src/main/java/com/phong/headfirst/Github7/DemoModel.java
@@ -0,0 +1,14 @@
+package com.phong.headfirst.Github7;
+
+import java.util.Date;
+
+/**
+ * Created by huynhducthanhphong on 1/15/16.
+ */
+public class DemoModel {
+    private static int nextId = 0;
+    String label;
+    Date dateTime;
+    String pathToImage;
+    int id = ++nextId;
+}
