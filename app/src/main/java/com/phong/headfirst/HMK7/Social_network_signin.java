9606627893c747a5bedffe3a00d7ae002af533f5
p7157568 <p7157568@gmail.com>
2016-01-21 19:49:30 +0700

    hello

--- app/src/main/java/com/phong/headfirst/HMK7/Social_network_signin.java
+++ app/src/main/java/com/phong/headfirst/HMK7/Social_network_signin.java
@@ -11,12 +11,15 @@ public class Social_network_signin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_network_signin);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_social_network_signin, menu);
         return true;
